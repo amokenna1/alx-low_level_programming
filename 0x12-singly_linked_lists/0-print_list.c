4b644ff1478e87691b4d323b41ea3a12e681c1a3
amokenna1 <amokenna@icloud.com>
2023-11-16 18:41:22 -0800

    0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,8 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t nodeCount = 0;
 
-	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +16,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		nodeCount++;
 	}
-	return (nelem);
+	return (nodeCount);
 }
